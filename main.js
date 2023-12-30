a0f88690031d6bd8a92affd68369e9ff9bc6aea1
Javier Nuñez <javiernu16@gmail.com>
2023-12-29 20:01:17 -0500

    Add the functions to change the spacing, the background-color and the blur

---
+++ main.js
@@ -0,0 +1,32 @@
+const initialSpacing = document.querySelector('#initial-spacing');
+const initialColor = document.querySelector('#initial-color');
+const initialBlur = document.querySelector('#initial-blur')
+const mainContainer = document.querySelector('#main-container');
+const image = document.querySelector('#image');
+
+
+initialSpacing.addEventListener('input', changePadding)
+
+initialColor.addEventListener('input', changeColor)
+
+initialBlur.addEventListener('input', changeBlur)
+
+function changePadding() {
+  let initialSpacingValue = initialSpacing.value;
+  mainContainer.style.padding = `${initialSpacingValue}px`;
+  console.log(initialSpacingValue);  
+}
+
+function changeColor() {
+  let initialColorValue = initialColor.value;
+  // console.log(initialColorValue);
+  mainContainer.style.setProperty('--base-color', initialColorValue)
+}
+
+function changeBlur() {
+  let initialBlurValue = initialBlur.value;
+  console.log(image);
+  image.style.setProperty('--blur', `${initialBlurValue}px`);
+}
+
+// initialSpacing.addEventListener('click', changeStyles())
